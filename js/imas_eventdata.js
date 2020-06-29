30a434f5afc693fb2fec20521246498703d0243f
Yukina <themunyang21@naver.com>
2020-06-29 10:09:59 +0900

    컨벤션 센터 열람 및 관련 기능 삭제

--- js/imas_eventdata.js
+++ js/imas_eventdata.js
@@ -75,22 +75,6 @@ var showTDAnniversaryEventData = {
   document.getElementById("rank_timestamp").innerHTML = entryDate+" "+entryTime;
  },
 
- goIdolBoard : function(boardType){
-  var selectedIdol = new Number(document.getElementById("idol_select").value);
-  if(selectedIdol == 0){alert("아이돌을 선택해주세요!!");return;}
-
-  var timeNow = new Date();
-  var dateChecksum = (timeNow.getFullYear() * 10000)+((timeNow.getMonth()+1) * 100)+timeNow.getDate();
-
-  if(boardType == 0){window.open("https://mltd.matsurihi.me/election/board/"+selectedIdol,"_blank");} // 배역 투표
-  else if(boardType == 1){ // 2주년 이벤트
-   if(dateChecksum >= 20190701 && dateChecksum < 20190714){window.open("https://mltd.matsurihi.me/events/92/boards/"+selectedIdol,"_blank");}
-   else if(dateChecksum >= 20200629 && dateChecksum < 20200712){window.open("https://mltd.matsurihi.me/events/142/boards/"+selectedIdol,"_blank");}
-   else{alert("이벤트 기간 중에만 열람 가능합니다.");return;}
-  }
-  else{alert("올바르지 않은 컨벤션 센터 종류입니다.");return;}
- },
-
  goIdolInfo : function(){
   var selectedIdol = new Number(document.getElementById("idol_select").value);
   if(selectedIdol == 0){alert("아이돌을 선택해주세요!!");return;}
